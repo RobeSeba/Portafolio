d0d93ffc528ebe35d2c4feda484627d94c80a3d3
Roberto Sebastian <roberto_m_ll@hotmail.com>
2025-11-28 15:08:54 -0500

    se agrego un espacio entre las tajetas y se grego logica js para renderizar mi nombre

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 const menu = document.getElementById("menu-navegacion")
+const nombre= document.getElementById("nombre")
+const dataNombre = "Hola, Soy Roberto Magino".split("")
+const velocidadEscritura = 100;
+
+function escribir(array){
+    for (let index = 0; index < array.length; index++) {
+        const retraso= velocidadEscritura * index;
+        setTimeout(()=>{
+            nombre.innerHTML += array[index];
+        },retraso)
+    }
+}
+
+
 window.addEventListener("scroll",()=>{
     const scrollPosition = window.scrollY;
     if (scrollPosition>0) {
         menu.style.opacity="0.5"
+
     }else{
         menu.style.opacity="1"
     }
     
 })
+
+escribir(dataNombre)
